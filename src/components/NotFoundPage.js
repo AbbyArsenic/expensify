f1bb8020c9608550b3334964da67b98105ea7cf7
Abby Johnson <brit13@gmail.com>
2018-07-15 22:52:52 -0700

    broke out main components

---
+++ src/components/NotFoundPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NotFoundPage = () => (
+  <div>404 - Not found! <Link to="/">Go home.</Link></div>
+);
+
+export default NotFoundPage;
--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,30 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Link, NavLink, Route, Switch } from 'react-router-dom';
-
-const ExpenseDashboardPage = () => (
-  <div>This is from my dashboard component</div>
-);
-const AddExpensePage = () => (
-  <div>This is from my add expense component</div>
-);
-const EditExpensePage = () => (
-  <div>This is from my edit expense component</div>
-);
-const HelpPage = () => (
-  <div>This is from my help component</div>
-);
-const NotFoundPage = () => (
-  <div>404 - Not found! <Link to="/">Go home.</Link></div>
-);
-const Header = () => (
-  <header>
-    <h1>Expensify</h1>
-    <NavLink to="/" exact activeClassName="is-active">Dashboard</NavLink>
-    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-    <NavLink to="/edit" activeClassName="is-active">Edit Espense</NavLink>
-    <NavLink to="/help" activeClassName="is-active">Help</NavLink>
-  </header>
-);
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import AddExpensePage from '../components/AddExpensePage';
+import EditExpensePage from '../components/EditExpensePage';
+import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
+import Header from '../components/Header';
+import HelpPage from '../components/HelpPage';
+import NotFoundPage from '../components/NotFoundPage';
 
 const AppRouter = () => (
   <BrowserRouter>
